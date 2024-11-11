c1659b18560daec9f90d12dc3349a1d99b058d20
ahmed <ahmedbabar0708@gmail.com>
2024-11-11 03:06:42 -0500

    Running Listing Page

--- src/main/java/codelook/jpa/view/MainViewController.java
+++ src/main/java/codelook/jpa/view/MainViewController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -51,6 +53,18 @@ public class MainViewController {
     public String newListingPage(Model model) {
         model.addAttribute("books", bookInfoRepo.findAll());
         return "newListing";
+    }
+
+    @PostMapping
+    public String addListing(@RequestParam String name,
+                             @RequestParam String description,
+                             @RequestParam Date DatePublished,
+                             @RequestParam Format format,
+                             @RequestParam BigDecimal DiscountedPrice
+
+                             ) {
+        listingInfoRepo.save(listingInfo);
+        return "redirect:/allBooks";
     }
 
     // New Book Page
